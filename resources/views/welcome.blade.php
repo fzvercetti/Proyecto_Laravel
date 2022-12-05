a792f855614525e1636f01692a244363ffaa0a77
fzvercetti <felipezamorabaldazo@gmail.com>
2022-12-04 23:52:04 -0600

    beta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -162,10 +162,97 @@ body {
     </div>
     <div class="card">
       <h2>Contacto</h2>
-      <h5>Pon aqui el contacto FELIPE</h5>
-      <div class="fakeimg" style="height:200px;"></div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <!-- Container for demo purpose -->
+<div class="container my-24 px-6 mx-auto">
+
+<!-- Section: Design Block -->
+<section class="mb-32 text-center text-gray-800">
+  <div class="max-w-[700px] mx-auto px-3 lg:px-6">
+    
+    <form>
+      <div class="form-group mb-6">
+        <input type="text" class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+          placeholder="Name">
+      </div>
+      <div class="form-group mb-6">
+        <input type="email" class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+          placeholder="Email address">
+      </div>
+      <div class="form-group mb-6">
+        <textarea class="
+          form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+        " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
+      </div>
+      <div class="form-group form-check text-center mb-6">
+        <input type="checkbox"
+          class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+          checked>
+      </div>
+      <button type="submit" class="
+        w-full
+        px-6
+        py-2.5
+        bg-blue-600
+        text-white
+        font-medium
+        text-xs
+        leading-tight
+        uppercase
+        rounded
+        shadow-md
+        hover:bg-blue-700 hover:shadow-lg
+        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg
+        transition
+        duration-150
+        ease-in-out">Send</button>
+    </form>
+  </div>
+</section>
+<!-- Section: Design Block -->
+
+</div>
+<!-- Container for demo purpose -->
     </div>
   </div>
   <div class="rightcolumn">
